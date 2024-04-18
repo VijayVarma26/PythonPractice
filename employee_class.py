739e71ef33ff7fbd92be03084f584571b80f754b
Vijay Varma <vijay.varma@iongroup.com>
2024-04-18 21:09:02 +0530

    Updated: employee class added extra print method and attr calls

--- employee_class.py
+++ employee_class.py
@@ -5,14 +5,28 @@ class Employee:
         self.age = age
         self.department = department
 
+    # returns the string representation of the object. Gets called when we use print() function
+    def __str__(self):
+        return "Name: {}, Age: {}, Department: {}".format(self.name, self.age, self.department)
+    
+    # prints all the employee attributes
     def display_info(self):
         print("Name:", self.name)
         print("Age:", self.age)
-        print("Department:", self.salary)
+        print("Department:", self.department)
 
 
 # Creating an object of Employee class
 emp1 = Employee("Vijay", 27, "IT")
 
-# Accessing attributes and methods of the object
+
+# Accessing attributes of the object
+print(emp1.name)
+print(emp1.age)
+print(emp1.department)
+
+# Accessing methods of the object
 emp1.display_info()
+
+# using __str__ method to display
+print(emp1)
